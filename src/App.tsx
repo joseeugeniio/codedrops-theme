811c04e4cffb041f7bc8bc0a99bdad53050e951d
joseeugeniio <jeugenioneves@gmail.com>
2023-04-07 17:00:30 -0300

    add save automatic page theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
 import Header from "./components/Header";
 import GlobalStyle from "./styles/global";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, DefaultTheme } from "styled-components";
 import light from "./styles/themes/light";
 import dark from "./styles/themes/dark";
+import usePersistedState from "./utils/usePersistedState";
 
 function App() {
-  const [theme, setTheme] = useState(light)
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('light', light)
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light)
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,9 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
     title: 'light',
 
     colors: {
-        primary: '#5BC0BE',
-        secondary: '#3A506B',
+        primary: '#3A506B',
+        secondary: '#5BC0BE',
 
         background: '#f5f5f5',
         text: '#333'
